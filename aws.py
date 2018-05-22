0aea8872238d31be2d186768d52cbdce356590a2
YatharthLakhera <17159994+YatharthLakhera@users.noreply.github.com>
2018-05-22 18:07:59 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -32,49 +32,52 @@ class EC2_sevices :
 
 			while (instance_status == "waiting" ) :
 
-				try :
+				print("Instance {0}({1}) : {2} (Time Taken : {3:.2f} seconds)\r".format(count, instance,
+				 instance_status, time.time()-start_time), end="")
 
-					print("Instance {0}({1}) : {2} (Time Taken : {3:.2f} seconds)\r".format(count, instance,
-					 instance_status, time.time()-start_time), end="")
+				instance_process = ""
+				instance_state = ""
 
-					instance_process = ""
-					instance_state = ""
+				# Executing aws cli for starting instance and getting response
+				if(process == "start") : 
+					instance_process = "start-instances"
+					response_ob1 = "StartingInstances"
+					response_ob2 = "CurrentState"
 
-					# Executing aws cli for starting instance and getting response
-					if(process == "start") : 
-						instance_process = "start-instances"
-						response_ob1 = "StartingInstances"
-						response_ob2 = "CurrentState"
+				# Executing aws cli for stopping instance and getting response
+				elif(process == "stop") :
+					instance_process = "stop-instances"
+					response_ob1 = "StoppingInstances"
+					response_ob2 = "CurrentState"
 
-					# Executing aws cli for stopping instance and getting response
-					elif(process == "stop") :
-						instance_process = "stop-instances"
-						response_ob1 = "StoppingInstances"
-						response_ob2 = "CurrentState"
+				# Executing aws cli for getting instance status
+				elif(process == "status") : 
+					instance_process = "describe-instance-status"
+					response_ob1 = "InstanceStatuses"
+					response_ob2 = "InstanceState"
 
-					# Executing aws cli for getting instance status
-					elif(process == "status") : 
-						instance_process = "describe-instance-status"
-						response_ob1 = "InstanceStatuses"
-						response_ob2 = "InstanceState"
+				else :
+					print("Invalid instance state given in function arguments!")
+					return
 
-					else :
-						print("Invalid instance state given in function arguments!")
-						return
-
+				try : 	
 					read = subprocess.run(['aws', 'ec2', instance_process,
-						'--instance-ids', instance], stdout=subprocess.PIPE)
-
-					output = json.loads(read.stdout.decode("utf-8"))
-					instance_status = output[response_ob1][0][response_ob2]["Name"]
-
-					if(instance_status == "pending") : 
-						instance_status = "started"
-						
-					print("Instance {0}({1}) : {2} (Time Taken : {3:.2f} seconds)".format(count, instance,
-					 instance_status, time.time()-start_time))
-				
+					'--instance-ids', instance], stdout=subprocess.PIPE)
 				except :
-					instance_status = "waiting"	
+					instance_status = "waiting"
 
+				output = json.loads(read.stdout.decode("utf-8"))
+				try :
+					instance_status = output[response_ob1][0][response_ob2]["Name"]
+				except IndexError:
+					print("Unexpected Response from AWS Service for instance : "+str(instance))
+					print(output)
+					exit(0)
+
+				if(instance_status == "pending") : 
+					instance_status = "started"
+					
+				print("Instance {0}({1}) : {2} (Time Taken : {3:.2f} seconds)".format(count, instance,
+				 instance_status, time.time()-start_time))
+				
 			count += 1
